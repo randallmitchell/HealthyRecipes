9b39c69a400994bf9450d6a425d2bab6ffc00477
Randall Mitchell <randallmitchell@paramount.com>
2024-05-10 18:31:24 -0400

    Add base color palette.

--- app/src/main/java/com/methodsignature/healthyrecipes/ui/theme/Color.kt
+++ app/src/main/java/com/methodsignature/healthyrecipes/ui/theme/Color.kt
@@ -9,3 +9,11 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+class Colors {
+    val Ceramic = Color(0xFF5C868E)
+    val Chutney = Color(0xFF7B7917)
+    val Corriander = Color(0xFFAD801C)
+    val Lotus = Color(0xFF894963)
+    val Saffron = Color(0xFFF0C00D)
+}g
