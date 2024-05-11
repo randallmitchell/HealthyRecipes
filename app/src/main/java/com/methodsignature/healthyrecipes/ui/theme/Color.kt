6fdca6430b50f177cd61561f630d28a16ccd37ca
Randall Mitchell <randallmitchell@paramount.com>
2024-05-10 23:40:56 -0400

    add colors preview and extra colors

--- app/src/main/java/com/methodsignature/healthyrecipes/ui/theme/Color.kt
+++ app/src/main/java/com/methodsignature/healthyrecipes/ui/theme/Color.kt
@@ -1,6 +1,20 @@
 package com.methodsignature.healthyrecipes.ui.theme
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyListScope
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 val Purple80 = Color(0xFFD0BCFF)
 val PurpleGrey80 = Color(0xFFCCC2DC)
@@ -10,10 +24,51 @@ val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
 
-class Colors {
+object Colors {
+    val Basmati = Color(0xFFFFFFFF)
+    val Naan = Color(0xFFFFFFF0)
     val Ceramic = Color(0xFF5C868E)
     val Chutney = Color(0xFF7B7917)
     val Corriander = Color(0xFFAD801C)
     val Lotus = Color(0xFF894963)
     val Saffron = Color(0xFFF0C00D)
-}g
+    val Steel = Color(0x3F303237)
+    val Tamarind = Color(0xFF000000)
+}
+
+@Preview
+@Composable
+fun ColorsPreview() {
+    val colors = listOf(
+        "Basmati" to Colors.Basmati,
+        "Naan" to Colors.Naan,
+        "Ceramic" to Colors.Ceramic,
+        "Chutney" to Colors.Chutney,
+        "Corriander" to Colors.Corriander,
+        "Lotus" to Colors.Lotus,
+        "Saffron" to Colors.Saffron,
+        "Steel" to Colors.Steel,
+        "Tamarind" to Colors.Tamarind,
+    )
+
+    LazyColumn {
+        items(colors.size) { i ->
+            Text(
+                text = colors[i].first,
+                color = if (
+                    colors[i].second == Colors.Steel ||
+                    colors[i].second == Colors.Tamarind
+                ) {
+                    Colors.Basmati
+                } else {
+                    Colors.Tamarind
+                },
+                textAlign = TextAlign.Center,
+                modifier = Modifier
+                    .background(colors[i].second)
+                    .padding(all = 20.dp)
+                    .fillParentMaxWidth()
+            )
+        }
+    }
+}
