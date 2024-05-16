fb2814b1ef4fa0a0a550e0b1c5cbb70544c2cb33
Randall Mitchell <randallmitchell@paramount.com>
2024-05-15 21:25:01 -0400

    add some structure to the splash screen

--- app/src/main/java/com/methodsignature/healthyrecipes/ui/features/onboarding/OnboardingFlow.kt
+++ app/src/main/java/com/methodsignature/healthyrecipes/ui/features/onboarding/OnboardingFlow.kt
@@ -1,10 +1,11 @@
 package com.methodsignature.healthyrecipes.ui.features.onboarding
 
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import com.methodsignature.healthyrecipes.ui.features.onboarding.screens.SplashScreen
 
 @Composable
 fun OnboardingFlow(modifier: Modifier = Modifier) {
-    SplashScreen(modifier = modifier)
+    SplashScreen()
 }
