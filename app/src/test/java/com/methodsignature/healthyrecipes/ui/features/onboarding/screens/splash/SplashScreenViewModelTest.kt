fb657f9ddbeff12301a353bb66ea153c0cf8889b
Randall Mitchell <randallmitchell@paramount.com>
2024-05-23 13:51:45 -0400

    add splash screen test

--- app/src/test/java/com/methodsignature/healthyrecipes/ui/features/onboarding/screens/splash/SplashScreenViewModelTest.kt
+++ app/src/test/java/com/methodsignature/healthyrecipes/ui/features/onboarding/screens/splash/SplashScreenViewModelTest.kt
@@ -2,40 +2,48 @@ package com.methodsignature.healthyrecipes.ui.features.onboarding.screens.splash
 
 import app.cash.turbine.test
 import com.methodsignature.healthyrecipes.BaseTest
-import com.methodsignature.healthyrecipes.MainDispatcherRule
 import com.methodsignature.healthyrecipes.ui.features.onboarding.screens.splash.SplashScreenViewModel.NavigationEvent
 import com.methodsignature.healthyrecipes.ui.features.onboarding.screens.splash.SplashScreenViewModel.UiState
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.test.runTest
 import org.amshove.kluent.shouldBeEqualTo
-import org.junit.Rule
+import kotlin.math.exp
 import kotlin.test.Test
 
-class SplashScreenViewModelTest: BaseTest() {
+class SplashScreenViewModelTest : BaseTest() {
 
     @Test
-    fun onSplashStart_sendSplashCompleteEvent() =
-        runTest {
-            // GIVEN the Splash screen is launched
-            val tested = SplashScreenViewModel()
+    fun onSplashCreated_sendUiDisplayingState() = runTest {
+        // GIVEN the Splash screen is created
+        val tested = SplashScreenViewModel()
 
-            // THEN SplashComplete is called after some seconds
-            tested.uiState.test {
-                delay(5000)
-                expectMostRecentItem() shouldBeEqualTo UiState.Navigating(NavigationEvent.SplashComplete)
-            }
+        // THEN Displaying event is sent
+        tested.uiState.test {
+            expectMostRecentItem() shouldBeEqualTo UiState.Displaying
         }
+    }
 
     @Test
-    fun onSplashNavigated_sendNavigatedEvent() =
-        runTest {
-            // GIVEN onNavigated is called
-            val tested = SplashScreenViewModel()
-            tested.onNavigated()
+    fun onSplashStart_sendSplashCompleteEvent() = runTest {
+        // GIVEN the Splash screen is launched
+        val tested = SplashScreenViewModel()
 
-            // THEN Navigated is called
-            tested.uiState.test {
-                expectMostRecentItem() shouldBeEqualTo UiState.Navigated
-            }
+        // THEN SplashComplete is called after some seconds
+        tested.uiState.test {
+            delay(5000)
+            expectMostRecentItem() shouldBeEqualTo UiState.Navigating(NavigationEvent.SplashComplete)
         }
+    }
+
+    @Test
+    fun onSplashNavigated_sendNavigatedEvent() = runTest {
+        // GIVEN onNavigated is called
+        val tested = SplashScreenViewModel()
+        tested.onNavigated()
+
+        // THEN Navigated is called
+        tested.uiState.test {
+            expectMostRecentItem() shouldBeEqualTo UiState.Navigated
+        }
+    }
 }
