0f6c56e60a196c5b6852e5bf5f8794b13f697666
Randall Mitchell <randallmitchell@paramount.com>
2024-05-28 17:27:05 -0400

    add more unit tests for recipe list view model

--- app/src/test/java/com/methodsignature/healthyrecipes/ui/flows/content/recipelist/RecipeListViewModelTest.kt
+++ app/src/test/java/com/methodsignature/healthyrecipes/ui/flows/content/recipelist/RecipeListViewModelTest.kt
@@ -8,6 +8,7 @@ import com.methodsignature.healthyrecipes.value.NonBlankString
 import io.mockk.coEvery
 import io.mockk.mockk
 import kotlinx.coroutines.test.runTest
+import org.amshove.kluent.shouldBeEqualTo
 import org.amshove.kluent.shouldBeInstanceOf
 import org.junit.Test
 
@@ -20,7 +21,17 @@ class RecipeListViewModelTest : BaseTest() {
             unitType = NonBlankString.from("cups")!!,
             name = NonBlankString.from("magic")!!,
         )
-        val ingredients = listOf(ingredient)
+        val ingredient2 = GetRecipeListUseCase.Ingredient(
+            units = NonBlankString.from("1")!!,
+            unitType = NonBlankString.from("tablespoon")!!,
+            name = NonBlankString.from("love")!!,
+        )
+        val ingredient3 = GetRecipeListUseCase.Ingredient(
+            units = NonBlankString.from("1")!!,
+            unitType = NonBlankString.from("piece")!!,
+            name = NonBlankString.from("determination")!!,
+        )
+        val ingredients = listOf(ingredient, ingredient2, ingredient3)
 
         val recipe = GetRecipeListUseCase.Recipe(
             id = NonBlankString.from("1")!!,
@@ -43,7 +54,30 @@ class RecipeListViewModelTest : BaseTest() {
 
         // THEN the recipe list is shown
         tested.uiState.test {
-            expectMostRecentItem() shouldBeInstanceOf UiState.RecipeList::class.java
+            val item = awaitItem()
+
+            item shouldBeInstanceOf UiState.RecipeList::class.java
+
+            val recipes = (item as UiState.RecipeList).recipeList
+            recipes.size shouldBeEqualTo 1
+        }
+    }
+
+    @Test
+    fun onRecipeListReceived_concatenatesIngredients() = runTest {
+        // GIVEN the recipe list is returned
+        val useCase = mockk<GetRecipeListUseCase>()
+        coEvery { useCase.run() } returns (TestData.recipes)
+        val tested = RecipeListViewModel(
+            recipesUseCase = useCase,
+        )
+
+        // THEN the recipe ingredients are concatenated
+        tested.uiState.test {
+            val item = awaitItem()
+            item shouldBeInstanceOf UiState.RecipeList::class.java
+            val recipe = (item as UiState.RecipeList).recipeList[0]
+            recipe.body?.value shouldBeEqualTo "magic, love, determination"
         }
     }
 }
