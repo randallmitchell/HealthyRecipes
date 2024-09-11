55e4e1889878d6ed80bd44d0213b474e904be610
Randall Mitchell <rmdev@methodsignature.com>
2024-09-11 11:26:43 -0400

    16 Coroutine Dispatcher Cleanup (#20)

--- app/src/main/java/com/methodsignature/healthyrecipes/service/configuration/DataStoreConfigurationService.kt
+++ app/src/main/java/com/methodsignature/healthyrecipes/service/configuration/DataStoreConfigurationService.kt
@@ -7,28 +7,34 @@ import androidx.datastore.preferences.core.edit
 import androidx.datastore.preferences.core.intPreferencesKey
 import androidx.datastore.preferences.preferencesDataStore
 import com.methodsignature.healthyrecipes.service._api.configuration.ConfigurationService
+import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 class DataStoreConfigurationService @Inject constructor(
-    val context: Context
+    private val context: Context,
+    private val ioDispatcher: CoroutineDispatcher,
 ) : ConfigurationService {
 
     private val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = DATA_STORE_NAME)
 
-    override suspend fun getRecipeSeedState(): Flow<ConfigurationService.RecipeSeedState> {
-        return context.dataStore.data.map { preferences ->
-            val raw = preferences[KEY_RECIPE_SEED_STATE] ?: 0
-            ConfigurationService.RecipeSeedState.fromInt(raw)
+    override suspend fun getRecipeSeedState(): Flow<ConfigurationService.RecipeSeedState> =
+        withContext(ioDispatcher) {
+            context.dataStore.data.map { preferences ->
+                val raw = preferences[KEY_RECIPE_SEED_STATE] ?: 0
+                ConfigurationService.RecipeSeedState.fromInt(raw)
+            }
         }
-    }
 
-    override suspend fun setRecipeSeedState(value: ConfigurationService.RecipeSeedState) {
-        context.dataStore.edit {preferences ->
-            preferences[KEY_RECIPE_SEED_STATE] = value.toInt()
+    override suspend fun setRecipeSeedState(value: ConfigurationService.RecipeSeedState) =
+        withContext(ioDispatcher) {
+            context.dataStore.edit { preferences ->
+                preferences[KEY_RECIPE_SEED_STATE] = value.toInt()
+            }
+            Unit
         }
-    }
 
     companion object {
         private const val DATA_STORE_NAME = "settings"
--- app/src/main/java/com/methodsignature/healthyrecipes/service/recipe/RealmDbLocalRecipeService.kt
+++ app/src/main/java/com/methodsignature/healthyrecipes/service/recipe/RealmDbLocalRecipeService.kt
@@ -1,32 +1,35 @@
 package com.methodsignature.healthyrecipes.service.recipe
 
+import com.methodsignature.healthyrecipes.language.value.EntityId
+import com.methodsignature.healthyrecipes.service._api.errors.EntityNotFoundException
 import com.methodsignature.healthyrecipes.service._api.recipe.LocalRecipeService
 import com.methodsignature.healthyrecipes.service._api.recipe.model.Recipe
-import com.methodsignature.healthyrecipes.service._api.errors.EntityNotFoundException
 import com.methodsignature.healthyrecipes.service.recipe.model.RealmRecipe
 import com.methodsignature.healthyrecipes.service.recipe.model.RealmRecipe.Companion.toRecipe
-import com.methodsignature.healthyrecipes.language.value.EntityId
 import io.realm.kotlin.Realm
 import io.realm.kotlin.UpdatePolicy
 import io.realm.kotlin.ext.query
+import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 class RealmDbLocalRecipeService @Inject constructor(
     private val realm: Realm,
+    private val ioDispatcher: CoroutineDispatcher,
 ) : LocalRecipeService {
 
-    override suspend fun observeAllRecipes(): Flow<List<Recipe>> {
-        return realm.query<RealmRecipe>().find().asFlow().map { resultsChange ->
+    override suspend fun observeAllRecipes(): Flow<List<Recipe>> = withContext(ioDispatcher) {
+        realm.query<RealmRecipe>().find().asFlow().map { resultsChange ->
             resultsChange.list.map {
                 it.toRecipe()
             }
         }
     }
 
-    override suspend fun observeRecipe(id: EntityId): Flow<Recipe> {
-        return realm.query<RealmRecipe>(
+    override suspend fun observeRecipe(id: EntityId): Flow<Recipe> = withContext(ioDispatcher) {
+        realm.query<RealmRecipe>(
             "id == $0",
             id.value
         ).find().asFlow().map { resultsChange ->
@@ -37,16 +40,17 @@ class RealmDbLocalRecipeService @Inject constructor(
         }
     }
 
-    override suspend fun upsertRecipe(recipe: Recipe) {
+    override suspend fun upsertRecipe(recipe: Recipe) = withContext(ioDispatcher) {
         realm.writeBlocking {
             copyToRealm(
                 RealmRecipe.fromRecipe(recipe),
                 UpdatePolicy.ALL
             )
+            Unit
         }
     }
 
-    override suspend fun upsertRecipes(withRecipes: List<Recipe>) {
+    override suspend fun upsertRecipes(withRecipes: List<Recipe>) = withContext(ioDispatcher) {
         realm.writeBlocking {
             withRecipes.forEach {
                 copyToRealm(
@@ -57,7 +61,7 @@ class RealmDbLocalRecipeService @Inject constructor(
         }
     }
 
-    override suspend fun deleteAllRecipes() {
+    override suspend fun deleteAllRecipes() = withContext(ioDispatcher) {
         realm.writeBlocking {
             val allRecipes = query<RealmRecipe>().find()
             delete(allRecipes)
